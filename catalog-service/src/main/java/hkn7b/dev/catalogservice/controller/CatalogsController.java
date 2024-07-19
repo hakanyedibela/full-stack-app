68366ccf59b9702b4eb2066360fe60dbde36ded1
HKN <hakan.yedibela@gmail.com>
2024-07-19 14:14:21 +0200

    Update pom.xml to use mongodb as datasource

--- catalog-service/src/main/java/hkn7b/dev/catalogservice/controller/CatalogsController.java
+++ catalog-service/src/main/java/hkn7b/dev/catalogservice/controller/CatalogsController.java
@@ -1,11 +1,25 @@
 package hkn7b.dev.catalogservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CatalogsController {
 
+    @GetMapping(path = "/livenessProbe",
+            name = "Liveness Probe GET Endpoint",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getLivenessProbe() {
+        return new ResponseEntity<>("Liveness Probe successfully established.", HttpStatus.OK);
+    }
+
     @GetMapping(path = "/catalogs")
-    
+    public ResponseEntity<?> getAllCatalogs() {
+
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
 }
